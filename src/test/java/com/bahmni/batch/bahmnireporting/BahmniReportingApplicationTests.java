f0d7739b35931dae2207643ae710552d3a7bb359
Rajashri <jadhavrajashri49@gmail.com>
2017-11-09 13:04:49 +0530

    Initial commit - create empty spring application

---
+++ src/test/java/com/bahmni/batch/bahmnireporting/BahmniReportingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bahmni.batch.bahmnireporting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BahmniReportingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
