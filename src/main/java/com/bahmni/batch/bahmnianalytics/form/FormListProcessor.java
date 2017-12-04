e33ba9d39ec57974968b212f1f4b77789a58c639
Rajashri <jadhavrajashri49@gmail.com>
2017-12-04 13:53:56 +0530

    Rajashri | remove commented code and format file

--- src/main/java/com/bahmni/batch/bahmnianalytics/form/FormListProcessor.java
+++ src/main/java/com/bahmni/batch/bahmnianalytics/form/FormListProcessor.java
@@ -13,47 +13,42 @@ import java.util.List;
 @Component
 public class FormListProcessor {
 
-	public static final String ALL_FORMS = "Post-operative Anaesthesia Note";
+    public static final String ALL_FORMS = "Post-operative Anaesthesia Note";
 
-	@Autowired
-	private ObsService obsService;
+    @Autowired
+    private ObsService obsService;
 
-	@Autowired
-	private BahmniFormFactory bahmniFormFactory;
+    @Autowired
+    private BahmniFormFactory bahmniFormFactory;
 
 
-	public List<BahmniForm> retrieveAllForms(){
-		List<Concept> allFormConcepts = obsService.getConceptsByNames(ALL_FORMS);
-//		allFormConcepts.add(obsService.getConceptsByNames("Bacteriology Concept Set").get(0));
+    public List<BahmniForm> retrieveAllForms() {
+        List<Concept> allFormConcepts = obsService.getConceptsByNames(ALL_FORMS);
 
-		List<BahmniForm> forms = new ArrayList<>();
-		for(Concept concept: allFormConcepts){
-			forms.add(bahmniFormFactory.createForm(concept,null));
-		}
+        List<BahmniForm> forms = new ArrayList<>();
+        for (Concept concept : allFormConcepts) {
+            forms.add(bahmniFormFactory.createForm(concept, null));
+        }
 
-		List<BahmniForm> flattenedFormList = new ArrayList<>(forms);
-		fetchExportFormsList(forms, flattenedFormList);
-//		for (BahmniForm form : flattenedFormList) {
-//			obsService.createTables(form);
-//		}
+        List<BahmniForm> flattenedFormList = new ArrayList<>(forms);
+        fetchExportFormsList(forms, flattenedFormList);
+        return flattenedFormList;
+    }
 
-		return flattenedFormList;
-	}
+    private void fetchExportFormsList(List<BahmniForm> forms, List<BahmniForm> flattenedList) {
+        for (BahmniForm form : forms) {
+            if (form.getChildren().size() != 0) {
+                flattenedList.addAll(form.getChildren());
+                fetchExportFormsList(form.getChildren(), flattenedList);
+            }
+        }
+    }
 
-	private void fetchExportFormsList(List<BahmniForm> forms, List<BahmniForm> flattenedList){
-		for(BahmniForm form : forms){
-			if(form.getChildren().size()!=0) {
-				flattenedList.addAll(form.getChildren());
-				fetchExportFormsList(form.getChildren(), flattenedList);
-			}
-		}
-	}
+    public void setObsService(ObsService obsService) {
+        this.obsService = obsService;
+    }
 
-	public void setObsService(ObsService obsService) {
-		this.obsService = obsService;
-	}
-
-	public void setBahmniFormFactory(BahmniFormFactory bahmniFormFactory) {
-		this.bahmniFormFactory = bahmniFormFactory;
-	}
+    public void setBahmniFormFactory(BahmniFormFactory bahmniFormFactory) {
+        this.bahmniFormFactory = bahmniFormFactory;
+    }
 }
