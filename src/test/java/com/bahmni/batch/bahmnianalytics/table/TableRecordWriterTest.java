0260486c031c5c9e483035d591c62e2fb8bccef4
= <=>
2017-12-22 17:41:46 +0530

    Prasanna, Rajashri | # | Add tests for Table data processor and writer

---
+++ src/test/java/com/bahmni/batch/bahmnianalytics/table/TableRecordWriterTest.java
@@ -0,0 +1,66 @@
+package com.bahmni.batch.bahmnianalytics.table;
+
+import com.bahmni.batch.bahmnianalytics.helper.FreeMarkerEvaluator;
+import com.bahmni.batch.bahmnianalytics.table.domain.TableData;
+import com.bahmni.batch.bahmnianalytics.util.BatchUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+public class TableRecordWriterTest {
+
+    @Mock
+    private JdbcTemplate postgresJdbcTemplate;
+
+    @Mock
+    private FreeMarkerEvaluator<TableRecordHolder> tableRecordHolderFreeMarkerEvaluator;
+
+    TableRecordWriter tableRecordWriter;
+    Map<String, Object> items;
+    TableData tableData;
+
+    @Before
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(BatchUtils.class);
+        tableRecordWriter = new TableRecordWriter();
+        items = new HashMap<String, Object>() {
+            {
+                put("program_id", 123);
+            }
+        };
+        tableData = new TableData();
+        tableData.setName("program");
+        tableRecordWriter.setTableData(tableData);
+        setValuesForMemberFields(tableRecordWriter, "postgresJdbcTemplate", postgresJdbcTemplate);
+        setValuesForMemberFields(tableRecordWriter, "tableRecordHolderFreeMarkerEvaluator", tableRecordHolderFreeMarkerEvaluator);
+    }
+
+    @Test
+    public void shouldExecuteEvaluatedInsertSql() throws Exception {
+        String sql = "";
+        when(tableRecordHolderFreeMarkerEvaluator.evaluate(anyString(), any(TableRecordHolder.class))).thenReturn(sql);
+
+        tableRecordWriter.write(Arrays.asList(items));
+
+        verify(postgresJdbcTemplate, times(1)).execute(sql);
+        verify(tableRecordHolderFreeMarkerEvaluator, times(1)).evaluate(anyString(), any(TableRecordHolder.class));
+    }
+
+    private void setValuesForMemberFields(Object observationExportStep, String fieldName, Object valueForMemberField) throws NoSuchFieldException, IllegalAccessException {
+        Field f1 = observationExportStep.getClass().getDeclaredField(fieldName);
+        f1.setAccessible(true);
+        f1.set(observationExportStep, valueForMemberField);
+    }
+}
