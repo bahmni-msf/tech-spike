786ae511c348cdd965bab6f93beba1f34576aed7
= <=>
2018-01-05 10:47:17 +0530

    Mahitha, Praasnna | # | [Fix] Get concept names instead of coded values

--- src/main/resources/sql/leafObs.sql
+++ src/main/resources/sql/leafObs.sql
@@ -3,7 +3,7 @@ SELECT
   o.person_id                      AS patientId,
   o.concept_id                     AS conceptId,
   o.obs_id                         AS id,
-  coalesce(DATE_FORMAT(o.value_datetime, '%d/%b/%Y'), o.value_numeric, o.value_text, cv.code, cvn.concept_full_name,
+  coalesce(DATE_FORMAT(o.value_datetime, '%d/%b/%Y'), o.value_numeric, o.value_text, cvn.concept_full_name,
            cvn.concept_short_name) AS value,
   obs_con.concept_full_name        AS conceptName,
   parent_obs_con.concept_full_name AS parentConceptName
@@ -13,9 +13,6 @@ FROM  obs o
   LEFT OUTER JOIN obs as parent_obs on parent_obs.obs_id = form_obs.obs_group_id and form_obs.voided = 0
   LEFT OUTER JOIN concept_view parent_obs_con ON parent_obs.concept_id = parent_obs_con.concept_id
   LEFT OUTER JOIN concept codedConcept ON o.value_coded = codedConcept.concept_id
-  LEFT OUTER JOIN concept_reference_term_map_view cv
-    ON cv.concept_id = codedConcept.concept_id AND cv.concept_map_type_name = 'SAME-AS' AND
-       cv.concept_reference_source_name = 'MSF-INTERNAL'
   LEFT OUTER JOIN concept_view cvn ON codedConcept.concept_id = cvn.concept_id
 WHERE
   o.obs_id IN (:childObsIds)
